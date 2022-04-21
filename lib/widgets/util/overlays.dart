889889597a81b4a58e63ce46e2ca0cdd0bc0ebb7
SoPlays <joao.ranger@gmail.com>
2022-04-21 22:50:57 +0100

    new Network Overlay implementation using snackbar removed puzzlepage unused comment changed scanned_list_page.dart messageStyle

--- lib/widgets/util/overlays.dart
+++ lib/widgets/util/overlays.dart
@@ -6,41 +6,23 @@ import 'package:logger/logger.dart';
 
 Future<void> showNetworkErrorOverlay(
     BuildContext context, Logger logger) async {
-  OverlayState? overlayState = Overlay.of(context);
-  OverlayEntry overlayEntry = OverlayEntry(
-    builder: (context) {
-      return Positioned(
-        top: 100,
-        right: 10,
-        child: Material(
-          elevation: 4.0,
-          child: Container(
-            decoration: BoxDecoration(
-                color: Theme.of(context).errorColor,
-                border: Border.all(color: Colors.black)),
-            child: Center(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(AppLocalizations.of(context)!.networkError),
-                    const Icon(Icons.signal_wifi_connected_no_internet_4),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      );
-    },
-    maintainState: true,
-    opaque: false,
-  );
-  overlayState?.insert(overlayEntry);
   logger.i("Inserted Network error overlay");
-  await Future.delayed(const Duration(seconds: 2));
-  overlayEntry.remove();
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      backgroundColor: Theme.of(context).errorColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+      ),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Text(AppLocalizations.of(context)!.networkError),
+          const Icon(Icons.signal_wifi_connected_no_internet_4),
+        ],
+      ),
+    ),
+  );
   logger.i("Removed Network error overlay");
 }
 
