81f88dcd8401b6ec3ff9923e980ffc56d278dea8
SoPlays <joao.ranger@gmail.com>
2022-04-26 14:26:59 +0100

    fixed invalid credentials flow

--- lib/pages/login/login_openday_page.dart
+++ lib/pages/login/login_openday_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/widgets.dart';
 import 'package:iscte_spots/models/auth/login_form_result.dart';
 import 'package:iscte_spots/services/openday/openday_login_service.dart';
 import 'package:iscte_spots/widgets/nav_drawer/page_routes.dart';
+import 'package:iscte_spots/widgets/util/loading.dart';
 
 import '../../widgets/util/iscte_theme.dart';
 import '../home.dart';
@@ -19,21 +20,16 @@ class LoginOpendayPage extends StatefulWidget {
 class _LoginOpendayState extends State<LoginOpendayPage> {
   final _loginFormkey = GlobalKey<FormState>();
 
-  @override
-  Widget build(BuildContext context) {
-    List<Widget> formFields = generateFormFields();
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login OpenDay"),
-      ),
-      body: Form(
-        key: _loginFormkey,
-        child:
-            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-          ...formFields,
-        ]),
-      ),
-    );
+  bool _loginError = false;
+  bool _isLoading = false;
+
+  String? loginValidator(String? value) {
+    if (_loginError) {
+      return "Invalid Credentials";
+    } else if (value == null || value.isEmpty) {
+      return 'Please enter some text';
+    }
+    return null;
   }
 
   List<Widget> generateFormFields() {
@@ -45,10 +41,7 @@ class _LoginOpendayState extends State<LoginOpendayPage> {
         decoration: IscteTheme.buildInputDecoration(hint: "userName"),
         textInputAction: TextInputAction.next,
         validator: (value) {
-          if (value == null || value.isEmpty) {
-            return 'Please enter some text';
-          }
-          return null;
+          return loginValidator(value);
         },
       ),
       TextFormField(
@@ -57,27 +50,56 @@ class _LoginOpendayState extends State<LoginOpendayPage> {
         decoration: IscteTheme.buildInputDecoration(hint: "password"),
         textInputAction: TextInputAction.done,
         validator: (value) {
-          if (value == null || value.isEmpty) {
-            return 'Please enter some text';
-          }
-          return null;
+          return loginValidator(value);
         },
       ),
       ElevatedButton(
-          onPressed: () async {
-            if (_loginFormkey.currentState!.validate()) {
-              int statusCode = await OpenDayLogin.login(
-                LoginFormResult(
-                  username: widget.userNameController.text,
-                  password: widget.passwordController.text,
-                ),
-              );
-              if (statusCode == 200) {
-                PageRoutes.replacePushanimateToPage(context, page: Home());
-              }
+        child: const Text("Login"),
+        onPressed: () async {
+          setState(() {
+            _loginError = false;
+            _isLoading = true;
+          });
+          if (_loginFormkey.currentState!.validate()) {
+            int statusCode = await OpenDayLogin.login(
+              LoginFormResult(
+                username: widget.userNameController.text,
+                password: widget.passwordController.text,
+              ),
+            );
+            if (statusCode == 200) {
+              PageRoutes.replacePushanimateToPage(context, page: Home());
+            } else {
+              setState(() {
+                _loginError = true;
+              });
             }
-          },
-          child: Text("Login")),
+          }
+          setState(() {
+            _isLoading = false;
+          });
+        },
+      ),
     ];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> formFields = generateFormFields();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login OpenDay"),
+      ),
+      body: _isLoading
+          ? const LoadingWidget()
+          : Form(
+              key: _loginFormkey,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ...formFields,
+                  ]),
+            ),
+    );
   }
 }
