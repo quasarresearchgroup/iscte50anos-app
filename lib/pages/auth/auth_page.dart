6be6182a19aa72616f681259376a2d52db8568e0
SoPlays <joao.ranger@gmail.com>
2022-04-29 22:21:32 +0100

    sucess screen with Lottie after login

--- lib/pages/auth/auth_page.dart
+++ lib/pages/auth/auth_page.dart
@@ -5,28 +5,34 @@ import 'package:iscte_spots/pages/auth/register/register_openday_page.dart';
 import 'package:iscte_spots/pages/home/openday_home.dart';
 import 'package:iscte_spots/services/auth/openday_login_service.dart';
 import 'package:iscte_spots/widgets/util/loading.dart';
+import 'package:logger/logger.dart';
+import 'package:lottie/lottie.dart';
 
 class AuthPage extends StatefulWidget {
   static const pageRoute = "/auth";
 
-  const AuthPage({Key? key}) : super(key: key);
+  final Logger _logger = Logger();
+
+  AuthPage({Key? key}) : super(key: key);
 
   @override
   State<AuthPage> createState() => _AuthPageState();
 }
 
-class _AuthPageState extends State<AuthPage>
-    with SingleTickerProviderStateMixin {
+class _AuthPageState extends State<AuthPage> with TickerProviderStateMixin {
   bool _isLoggedIn = true;
   bool _isLoading = true;
   late List<StatefulWidget> _pages;
   final int _loginIndex = 0;
   final int _registerIndex = 1;
   late TabController _tabController;
+  late final AnimationController _lottieController;
+  final animatedSwitcherDuration = const Duration(seconds: 1);
 
   @override
   void dispose() {
     _tabController.dispose();
+    _lottieController.dispose();
     super.dispose();
   }
 
@@ -35,11 +41,32 @@ class _AuthPageState extends State<AuthPage>
     super.initState();
     _pages = [
       LoginOpendayPage(
-          changeToSignUp: changeToSignUp, loggingComplete: loggingComplete),
+        changeToSignUp: changeToSignUp,
+        loggingComplete: loggingComplete,
+        animatedSwitcherDuration: animatedSwitcherDuration,
+      ),
       RegisterOpenDayPage(
-          changeToLogIn: changeToLogIn, loggingComplete: loggingComplete),
+        changeToLogIn: changeToLogIn,
+        loggingComplete: loggingComplete,
+        animatedSwitcherDuration: animatedSwitcherDuration,
+      ),
     ];
     _tabController = TabController(length: _pages.length, vsync: this);
+    _lottieController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+
+    _lottieController.addStatusListener(
+      (status) {
+        widget._logger.d("listenning to complete login animation $status");
+        if (status == AnimationStatus.completed) {
+          Future.delayed(const Duration(milliseconds: 500)).then(
+              (value) => Navigator.pushNamed(context, HomeOpenDay.pageRoute));
+        }
+      },
+    );
+
     initFunc();
   }
 
@@ -54,12 +81,8 @@ class _AuthPageState extends State<AuthPage>
     }
   }
 
-  void loggingComplete() {
-    setState(() {
-      _isLoggedIn = true;
-    });
-    //PageRoutes.animateToPage(context, page: HomeOpenDay());
-    Navigator.pushNamed(context, HomeOpenDay.pageRoute);
+  void loggingComplete() async {
+    setState(() => _isLoggedIn = true);
   }
 
   void changeToSignUp() {
@@ -74,17 +97,35 @@ class _AuthPageState extends State<AuthPage>
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: _isLoading
-          ? const LoadingWidget()
-          : _isLoggedIn
-              ? const Center(
-                  child: FlutterLogo(
-                  size: 300,
-                ))
-              : TabBarView(
-                  controller: _tabController,
-                  physics: NeverScrollableScrollPhysics(),
-                  children: _pages),
+      body: AnimatedSwitcher(
+        duration: animatedSwitcherDuration,
+        child: _isLoading
+            ? const LoadingWidget()
+            : _isLoggedIn
+                ? lottieCompleteLoginBuilder()
+                : TabBarView(
+                    controller: _tabController,
+                    physics: NeverScrollableScrollPhysics(),
+                    children: _pages,
+                  ),
+      ),
     );
   }
+
+  Widget lottieCompleteLoginBuilder() => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Lottie.network(
+            "https://assets6.lottiefiles.com/packages/lf20_Vwcw5D.json",
+            //width: MediaQuery.of(context).size.width * 0.5,
+            //height: MediaQuery.of(context).size.height * 0.5,
+            //fit: BoxFit.contain,
+            controller: _lottieController,
+            onLoaded: (LottieComposition composition) {
+              TickerFuture forward = _lottieController.forward();
+            },
+          )
+        ],
+      );
 }
