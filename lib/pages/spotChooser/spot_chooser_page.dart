242ec2a474aaf5a8f89b00fe6ae931e0f4673d50
joao <joao.ranger@gmail.com>
2022-08-13 17:31:51 +0100

    converted spot chooser page to use new model of spot

--- lib/pages/spotChooser/spot_chooser_page.dart
+++ lib/pages/spotChooser/spot_chooser_page.dart
@@ -4,9 +4,12 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:iscte_spots/models/database/tables/database_puzzle_piece_table.dart';
-import 'package:iscte_spots/pages/spotChooser/get_all_spots_service.dart';
+import 'package:iscte_spots/models/database/tables/database_spot_table.dart';
+import 'package:iscte_spots/models/spot.dart';
 import 'package:iscte_spots/services/shared_prefs_service.dart';
+import 'package:iscte_spots/services/spotChooser/fetch_all_spots_service.dart';
 import 'package:iscte_spots/widgets/util/loading.dart';
 import 'package:logger/logger.dart';
 
@@ -25,21 +28,46 @@ class SpotChooserPage extends StatefulWidget {
 
 class _SpotChooserPageState extends State<SpotChooserPage> {
   double blur = 10;
-  late Future<List<String>> future;
+  late Future<List<Spot>> future;
 
   @override
   void initState() {
     super.initState();
-    future = SpotsRequestService.getAllSpots(context: context);
+    future = DatabaseSpotTable.getAll();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder<List<String>>(
+      floatingActionButton: SpeedDial(
+          child: const Icon(
+            Icons.add,
+            color: Colors.white,
+          ),
+          children: [
+            SpeedDialChild(
+              backgroundColor: Theme.of(context).primaryColor,
+              child: const Icon(Icons.refresh),
+              onTap: () {
+                refreshCallback(context);
+              },
+            ),
+            SpeedDialChild(
+              backgroundColor: Theme.of(context).errorColor,
+              child: const Icon(Icons.delete),
+              onTap: () async {
+                await DatabaseSpotTable.removeALL();
+                setState(() {
+                  future = DatabaseSpotTable.getAll();
+                });
+              },
+            )
+          ]),
+      body: FutureBuilder<List<Spot>>(
         future: future,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
+            List<Spot> spotsList = snapshot.data!;
             return CustomScrollView(
                 physics: const BouncingScrollPhysics(),
                 slivers: [
@@ -49,26 +77,19 @@ class _SpotChooserPageState extends State<SpotChooserPage> {
                     floating: true,
                     snap: true,
                     stretch: true,
-                    onStretchTrigger: () async {
-                      widget._logger.d("fetching data");
-                      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                        content: Text("Refreshed"),
-                        duration: Duration(seconds: 2),
-                      ));
-                      future =
-                          SpotsRequestService.getAllSpots(context: context);
-                      setState(() {});
+                    onStretchTrigger: () {
+                      return refreshCallback(context);
                     },
                     expandedHeight: 150,
                     flexibleSpace: FlexibleSpaceBar(
                       background: DecoratedBox(
                           position: DecorationPosition.foreground,
                           decoration: const BoxDecoration(),
-                          child: buildTopRow(snapshot.data!)),
+                          child: buildTopRow(spotsList)),
                       collapseMode: CollapseMode.pin,
                     ),
                   ),
-                  buildSpotsGrid(snapshot.data!),
+                  buildSpotsGrid(spotsList),
                 ]);
           } else {
             return const LoadingWidget();
@@ -78,7 +99,18 @@ class _SpotChooserPageState extends State<SpotChooserPage> {
     );
   }
 
-  Widget buildTopRow(List<String> list) {
+  Future<void> refreshCallback(BuildContext context) async {
+    widget._logger.d("fetching data");
+    await SpotsRequestService.fetchAllSpots(context: context);
+    future = DatabaseSpotTable.getAll();
+    ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+      content: Text("Refreshed"),
+      duration: Duration(seconds: 2),
+    ));
+    setState(() {});
+  }
+
+  Widget buildTopRow(List<Spot> list) {
     double sliderMax = 20;
     double sliderMin = 0;
     return Column(
@@ -111,16 +143,37 @@ class _SpotChooserPageState extends State<SpotChooserPage> {
               return Colors.black;
             })),
             onPressed: () {
-              chooseSpot(list, Random().nextInt(list.length), context);
+              if (list.isNotEmpty) {
+                chooseSpot(list[Random().nextInt(list.length)], context);
+              } else {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    backgroundColor: Theme.of(context).errorColor,
+                    content: const Text("No Spots stored!")));
+              }
             },
-            child: Text(AppLocalizations.of(context)!.random)),
+            child: Text(
+              AppLocalizations.of(context)!.random,
+              style: TextStyle(color: Colors.white),
+            )),
       ],
     );
   }
 
-  Widget buildSpotsGrid(List<String> list) {
+  Widget buildSpotsGrid(List<Spot> list) {
     double spacing = 10;
 
+/*    if (list.isEmpty) {
+      return SliverList(
+        delegate: SliverChildListDelegate.fixed([
+          ListTile(
+            title: Text("No spots in list!"),
+          ),
+          ListTile(
+            title: Text("No spots in list!"),
+          ),
+        ]),
+      );
+    } else {*/
     return SliverGrid(
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
         mainAxisSpacing: spacing,
@@ -131,32 +184,35 @@ class _SpotChooserPageState extends State<SpotChooserPage> {
         (context, index) {
           return ClipRRect(
             borderRadius: BorderRadius.circular(20.0),
-            child: ImageFiltered(
-              imageFilter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
-              child: InkWell(
-                enableFeedback: true,
-                splashColor: Colors.black,
-                onTap: () {
-                  chooseSpot(list, index, context);
-                },
-                child: Container(
-                  decoration: const BoxDecoration(),
-                  child: Image.network(
-                    list[index],
-                    fit: BoxFit.cover,
+            child: list.isEmpty
+                ? const Center(child: Text("List is empty"))
+                : ImageFiltered(
+                    imageFilter: ImageFilter.blur(sigmaX: blur, sigmaY: blur),
+                    child: InkWell(
+                      enableFeedback: true,
+                      splashColor: Colors.black,
+                      onTap: () {
+                        chooseSpot(list[index], context);
+                      },
+                      child: Container(
+                        decoration: const BoxDecoration(),
+                        child: Image.network(
+                          list[index].photoLink,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-            ),
           );
         },
-        childCount: list.length,
+        childCount: list.isEmpty ? 1 : list.length,
       ),
     );
+    //}
   }
 
-  void chooseSpot(List<String> list, int index, BuildContext context) {
-    SharedPrefsService.storeCurrentPuzzleIMG(list[index]);
+  void chooseSpot(Spot spot, BuildContext context) {
+    SharedPrefsService.storeCurrentPuzzleIMG(spot.photoLink);
     DatabasePuzzlePieceTable.removeALL();
     Navigator.of(context).pop();
   }
