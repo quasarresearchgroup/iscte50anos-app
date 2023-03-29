1a002833c7055877dc36946670dad62169a3db5b
JoaoAlmeida-dev <joao.ranger@gmail.com>
2023-03-29 01:30:36 +0100

    Fix app launching into blank screen on release mode

--- lib/pages/auth/auth_initial_page.dart
+++ lib/pages/auth/auth_initial_page.dart
@@ -21,71 +21,69 @@ class AuthInitialPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Flexible(
-            flex: 9,
-            child: Column(
-                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    AppLocalizations.of(context)!.loginIscteHint,
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleMedium
-                        ?.copyWith(color: Colors.black),
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Flexible(
+          flex: 9,
+          child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  AppLocalizations.of(context)!.loginIscteHint,
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(color: Colors.black),
+                ),
+                DynamicTextButton(
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all(IscteTheme.iscteColor)),
+                  onPressed: _iscteLoginCallback,
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Icon(
+                        PlatformService.instance.isIos
+                            ? CupertinoIcons.lock
+                            : Icons.lock,
+                        color: Colors.white,
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        "Login Iscte", //TODO
+                        style: Theme.of(context)
+                            .textTheme
+                            .titleMedium
+                            ?.copyWith(color: Colors.white),
+                      ),
+                    ],
                   ),
-                  DynamicTextButton(
-                    style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all(IscteTheme.iscteColor)),
-                    onPressed: _iscteLoginCallback,
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Icon(
-                          PlatformService.instance.isIos
-                              ? CupertinoIcons.lock
-                              : Icons.lock,
-                          color: Colors.white,
-                        ),
-                        const SizedBox(width: 10),
-                        Text(
-                          "Login Iscte", //TODO
-                          style: Theme.of(context)
-                              .textTheme
-                              .titleMedium
-                              ?.copyWith(color: Colors.white),
-                        ),
-                      ],
-                    ),
-                  ),
-                ]),
-          ),
-          Flexible(
-            flex: 1,
-            child: DynamicTextButton(
-              style: const ButtonStyle(
-                  backgroundColor:
-                      MaterialStatePropertyAll(IscteTheme.iscteColor)),
-              onPressed: changeToLogIn,
-              child: Text(
-                "Admin Login",
-                style: Theme.of(context)
-                    .textTheme
-                    .titleMedium
-                    ?.copyWith(color: Colors.white),
-              ),
+                ),
+              ]),
+        ),
+        Flexible(
+          flex: 1,
+          child: DynamicTextButton(
+            style: const ButtonStyle(
+                backgroundColor:
+                    MaterialStatePropertyAll(IscteTheme.iscteColor)),
+            onPressed: changeToLogIn,
+            child: Text(
+              "Admin Login",
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium
+                  ?.copyWith(color: Colors.white),
             ),
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
